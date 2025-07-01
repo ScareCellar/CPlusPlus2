cdb116af7f69ef4257c0d15f782fe7981c3f932a
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-07-01 10:16:40 -0600

    day 2 C++2 changes

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace blood {
+	
+	constexpr float pi = 3.1415926535897932384626433832795f;
+	constexpr float twoPi = pi * 2;
+	constexpr float halfPi = pi / 2;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "Core/Math.h"
+
+#define NAME "Cameron\n"
 
 int main()
 
 {
-
+	
+	std::cout << NAME;
 	std::cout << "Hello,World!\n";
-
+	std::cout << blood::pi << std::endl;
 }
