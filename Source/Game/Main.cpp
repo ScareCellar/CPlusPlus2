6b6621cdc0c2e814ce757146b84b33b4f9c47307
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2025-07-11 09:38:38 -0600

    fixed right click line tool and added f to clear screen

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -92,17 +92,17 @@ int main(int argc, char* argv[]) {
             if (i != 0) {
                 if (dragDrawnPoints[i - 1].x != 0 && dragDrawnPoints[i - 1].y != 0 && dragDrawnPoints[i].x != 0 && dragDrawnPoints[i].y != 0) {
                     renderer.DrawLine(dragDrawnPoints[i].x, dragDrawnPoints[i].y, dragDrawnPoints[i-1].x, dragDrawnPoints[i-1].y);
-                    //renderer.SetColor(random::getRandomInt(256), random::getRandomInt(256), random::getRandomInt(256), 255);
-                    renderer.SetColor(255, 255, 255, 255);
+                    renderer.SetColor(random::getRandomInt(256), random::getRandomInt(256), random::getRandomInt(256), 255);
+                    //renderer.SetColor(255, 255, 255, 255);
                 }
             }
         }
 		
 
-        //code for drawing a single straight line in one color using right click
+        //code for drawing a single straight line in one random color using right click
         // currently crashes the application, so that's fun
-        /*if (input.GetMouseButtonDown(blood::InputSystem::MouseButton::Right)) {
-            while (input.GetMouseButtonPressed(blood::InputSystem::MouseButton::Right)) {
+        if (input.GetMouseButtonDown(blood::InputSystem::MouseButton::Right)) {
+            if (input.GetMouseButtonPressed(blood::InputSystem::MouseButton::Right)) {
                 lineToolStartPos = input.GetMousePosition();
             }
             renderer.DrawLine(lineToolStartPos.x, lineToolStartPos.y, input.GetMousePosition().x, input.GetMousePosition().y);
@@ -112,13 +112,13 @@ int main(int argc, char* argv[]) {
             lineToolEndPos = input.GetMousePosition(); 
             std::vector<vec2> drawnLine{ lineToolStartPos, lineToolEndPos };
             drawnLines.push_back(drawnLine);
-        }*/
+        }
 
         ////draw finalized line
-        /*for (int i = 0; i < drawnLines.size(); i++) {
+        for (int i = 0; i < drawnLines.size(); i++) {
             renderer.DrawLine(drawnLines[i][0].x, drawnLines[i][0].y, drawnLines[i][1].x, drawnLines[i][1].y);
 
-        }*/
+        }
 
         //for (vec2 star : stars) {
         //    renderer.SetColor(random::getRandomInt(256), random::getRandomInt(256), random::getRandomInt(256), 255); // Set render draw color to random color
@@ -131,6 +131,11 @@ int main(int argc, char* argv[]) {
         //    renderer.DrawLine(random::getRandomInt(1280) + random::getRandomFloat(), random::getRandomInt(1024) + random::getRandomFloat(), random::getRandomInt(1280) + random::getRandomFloat(), random::getRandomInt(1024) + random::getRandomFloat()); // Render a random line            
         //}
 
+
+        //shutdown when user presses escape button
+        if (input.GetKeyDown(SDL_SCANCODE_ESCAPE)) {
+            break;
+        }
         
 
         renderer.Present(); // Render the screen
